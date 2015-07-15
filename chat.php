f38a90430848d3c05b064568e0b701741a3c1aae
Lodell <huynh.laurent@gmail.com>
2015-07-15 15:55:39 +0200

    modif PDO à procédurale

--- chat.php
+++ chat.php
@@ -1,11 +1,14 @@
 <?php
 //requête pour récupérer les 10 derniers messages
-$requete = $bdd->query('SELECT login, message, DATE_FORMAT(date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_fr FROM message ORDER BY ID DESC LIMIT 0, 10');
+$sql = 'SELECT login, message, DATE_FORMAT(date, \'%d/%m/%Y à %Hh%imin%ss\') AS date_fr FROM message ORDER BY ID DESC LIMIT 0, 10';
+
+//Envoi de la requête
+$requete = mysql_query($sql) or die('Erreur affichage messages <br>'.$sql.'<br>'.mysql_error());
 
 //Boucle pour afficher les messages 
-while ($donnees = $requete->fetch())
+while ($donnees = mysql_fetch_assoc($requete))
 {
 	echo '<p><strong>' . htmlspecialchars($donnees['login']) . '</strong> le ' . htmlspecialchars($donnees['date_fr']) . ': ' . htmlspecialchars($donnees['message']) . '</p>';
 }
 
-$requete->closeCursor();
+mysql_close();
