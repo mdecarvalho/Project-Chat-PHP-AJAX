28eb953d373e402114332b043f636a854dd3e100
helaH <hela.soussii@gmail.com>
2015-07-15 15:57:39 +0200

    connexion a la bdd

---
+++ connexion.php
@@ -0,0 +1,9 @@
+<?php
+// connexion au serveur mysql
+$connect = mysqli_connect('localhost', 'root', '')or die ("erreur de connexion serveur");
+
+//selection de la bdd
+$bd= mysqli_select_db($connect, 'chat') or die ("erreur de connexion base");
+
+
+?>
