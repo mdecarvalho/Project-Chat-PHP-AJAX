6dcc2fdb37af5ad991f0dccb31b6afeb233210af
mdecarvalho <decarvalho.michael@gmail.com>
2015-07-15 15:00:30 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CHAT</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label>Pseudo<input type="text"></label>
+    </form>
+</body>
+</html>
