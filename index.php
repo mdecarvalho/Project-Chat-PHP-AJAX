7e229ea74d0cfc80e3d0577c61a24566d3216a5d
helaH <hela.soussii@gmail.com>
2015-07-16 16:08:28 +0200

    FIX bug

--- index.php
+++ index.php
@@ -7,22 +7,20 @@
 </head>
 <body>
     <?php
-        // on teste les données du formulaire
-        if (isset($_POST['nickname']) && isset($_POST['message'])) {
-            // envoie du message sur la bdd
-            date_default_timezone_set('UTC');
-            $date= date();
-            echo $date;
-            $sql = "INSERT INTO message (message, date, login) VALUES ('$message','$date','$login')";
-            mysql_query($sql) or die('Erreur d\'envoye de message'); 
+        session_start();
+        if($_SESSION['login'] == ''){
+            header ('location: login.php');
         }
     ?>
+    <?php
+       require_once("upload_message.php");
+    ?>
+    <a href="deconnexion.php" id="logout">Deconnexion</a>
     <form action="index.php" method="post">
         <section id="message-section">
             <label>
                 Pseudo:
                 <?php
-                    session_start();
                    $pseudo = $_SESSION['login'];
                     echo $pseudo;            
                 ?>
@@ -31,39 +29,7 @@
             <INPUT type="submit" name="submit-button" id="submit-button" value="Envoyer">
         </section><!-- message.section -->
     </form>
-
-    <?php 
-        require_once("upload_message.php");
-    session_start();
-    if($_SESSION['login'] == ''){
-        header ('location: login.php');
-    }   
-    ?>
-    <?php 
-        require_once("upload_message.php");
-        require_once("chat.php");
-    ?>
-    <a href="deconnexion.php" id="logout">Deconnexion</a>
-    <section id="message-section">
-        <div>
-            <form action="index.php" method="post">
-                <label>
-                    Pseudo:
-                    <?php
-
-                        $nickname = $_SESSION['login'];
-                        echo $nickname;            
-                    ?>
-                </label>
-                <textarea rows="4" cols="50" placeholder="Saisissez votre message" id="message" name="message"></textarea>
-                <input type="hidden" name="nickname" value="<?php echo $nickname ?>">
-                <INPUT type="submit" name="submit-button" id="submit-button" value="Envoyer">
-            </form>
-        </div>
-    </section><!-- message.section -->
-
     <script src="js/jquery.js"></script>
     <script src="js/script.js"></script>
-
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-   <html>
-   <head>
-       <script>
-       $('#envoi').click(function(e){
-
-    e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
-
-
-    var login = encodeURIComponent( $('#login').val() ); // on sécurise les données
-
-    var password = encodeURIComponent( $('#password').val() );
-
-
-    if(login != "" && password != ""){ // on vérifie que les variables ne sont pas vides
-
-        $.ajax({
-
-            url : "veriflogin.php", // on donne l'URL du fichier de traitement
-
-            type : "POST", // la requête est de type POST
-
-            data : "login=" + login + "&password=" + password // et on envoie nos données
-
-        });
-
-
-    }
-
-});
-       </script>
-   </head>
-    <body>
-       
-        <form action="verifLogin.php" method="POST">
-            <label>Login:</label>
-            <input type="name" name="login"/><br>
-            <label>password:</label>
-            <input type="password" name="password"/><br>
-            <input type="submit" value="connexion" id="envoi"/>
-        </form>
-    </body>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -59,5 +16,4 @@
     <script src="js/jquery.js"></script>
     <script src="js/script.js"></script>
 </body>
->>>>>>> origin/master
 </html>
