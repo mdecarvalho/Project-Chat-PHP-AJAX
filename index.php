d80b0b8b9f6940c0e5cf341c71f55aa2243fd4eb
mdecarvalho <decarvalho.michael@gmail.com>
2015-07-16 09:57:57 +0200

    Add css file + design

--- index.php
+++ index.php
@@ -3,18 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>CHAT</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/> 
 </head>
 <body>
     <form action="index.php" method="post">
-        <label>
-            Pseudo:
-            <?php
-                $pseudo = "test";
-                echo $pseudo;            
-            ?>
-            <input type="text" name="chat-message"  maxlength="500">
+        <section id="message-section">
+            <label>
+                Pseudo:
+                <?php
+                    $pseudo = "test";
+                    echo $pseudo;            
+                ?>
+            </label>
+            <textarea rows="4" cols="50" placeholder="Saisissez votre message" id="message"></textarea>
             <INPUT type="submit" name="submit-button" id="submit-button" value="Envoyer">
-        </label>
+        </section><!-- message.section -->
     </form>
 </body>
 </html>
